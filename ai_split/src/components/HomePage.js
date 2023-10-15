fca306380f062f6acdfcf01c52750e5eeab893c5
kanvkc-28 <kanvkc28@gmail.com>
2023-10-15 17:21:06 -0500

    logout

--- ai_split/src/components/HomePage.js
+++ ai_split/src/components/HomePage.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import { signOut } from "firebase/auth";
+import { auth } from "../firebase";
+import { useNavigate } from "react-router-dom";
 
-const HomePage = () => {
+function HomePage() {
+  const history = useNavigate();
+  const handleClick = () => {
+    signOut(auth).then((val) => {
+      console.log(val, "val");
+      history("/login");
+    });
+  };
   return (
-    <div>HomePage</div>
-  )
+    <div>
+      <div>HomePage</div>
+
+      <button onClick={handleClick}>signOut</button>
+    </div>
+  );
 }
 
-export default HomePage
+export default HomePage;
