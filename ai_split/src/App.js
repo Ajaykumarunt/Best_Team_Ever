efbe5d7d9358fe94bd73e4e75af449273c38aaf1
Ajaykumarunt <Ajaykumar.unt@gmail.com>
2023-10-15 15:30:30 -0500

    Home Page

--- ai_split/src/App.js
+++ ai_split/src/App.js
@@ -1,12 +1,68 @@
+// App.js
+//import React from 'react';
 import './App.css';
-import db from './firebase'
+//import './person'
+import myImage from './person-icon.jpg';
+import add from './add.png';
+import Member_1 from './member1.png';
+import Member_2 from './member2.png';
+import Member_3 from './member3.png';
 
 function App() {
   return (
-    <div className="App">
-     <h1 className='text-xl'>AI Split APP</h1>
+    <div className="app">
+      <ProfileComponent />
+      <BalanceComponent amount={70.11} />
+      
+      <MemberComponent name="Member 1" amount={23.43} />
+      
     </div>
   );
 }
 
+function ProfileComponent() {
+  return (
+    <div className="profile">
+      <img src={myImage} alt="Profile" />
+      <p>Hello, UserName</p>
+    </div>
+  );
+}
+
+function BalanceComponent({ amount }) {
+  return (
+    <div className="mai">
+    <div className="balance">
+      <p>you get back<br></br> ${amount.toFixed(2)}</p>  
+    </div>
+    
+    <div className="butt">
+    {/* < /><br></br> */}
+      <button >
+        <img src={add} alt="+" />Add Split
+        </button>
+        
+
+    </div>
+    </div>
+  );
+}
+
+
+const members = ["Member_1"];
+const mempic = [Member_1, Member_2, Member_3];
+
+function MemberComponent({name,amount}){
+
+return mempic.map(mempic => (
+    <div className="Mem" >
+        < img src={mempic} alt="Icon" className ="mempic" />
+        <p>{members}</p>
+        <p>You owe $23.43</p>
+    </div>
+));
+}
+
+
 export default App;
+
