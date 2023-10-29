ee86383654098f2f9f96b0894ebee6063186fa86
BhanuMotupalli <bhanu.motupalli7@gmail.com>
2023-10-28 20:15:19 -0500

    Groups page layout issue

--- ai_split/src/components/Groups.js
+++ ai_split/src/components/Groups.js
@@ -6,7 +6,7 @@ import { AuthContext } from "../contexts/AuthContext";
 import SideNavigation from "./SideNavigation";
 
 import { getFirestore, collection, getDocs } from "firebase/firestore";
-import app from '../firebase.js';
+import app from "../firebase.js";
 
 function Groups() {
   const { setcurrentUser } = useContext(AuthContext);
@@ -48,8 +48,9 @@ function Groups() {
     console.log(selectedUsers);
   };
 
-  const filteredNames = userNames.filter(name => 
-    searchValue && name.toLowerCase().includes(searchValue.toLowerCase())
+  const filteredNames = userNames.filter(
+    (name) =>
+      searchValue && name.toLowerCase().includes(searchValue.toLowerCase())
   );
 
   return (
@@ -57,67 +58,70 @@ function Groups() {
       <div className="w-1/4">
         <SideNavigation />
       </div>
-
-      <div className="w-3/4 p-8">
-        <div className="flex justify-between items-center mb-12">
-          <h2 className="text-4xl font-bold">Groups</h2>
-          <button
-            className="bg-green-500 text-white px-5 py-5 rounded"
-            onClick={() => setShowSearchBox(!showSearchBox)}
-          >
-            + Create
-          </button>
-        </div>
-
-        {showSearchBox && (
-          <div className="relative">
-            <input
-              type="text"
-              value={searchValue}
-              onChange={(e) => setSearchValue(e.target.value)}
-              placeholder="Search users..."
-              className="w-full px-4 py-2 rounded border shadow-md mb-4"
-            />
-            {filteredNames.length > 0 && (
-              <div className="absolute top-full w-full max-h-64 overflow-y-auto border">
-                <ul>
-                  {filteredNames.map(name => (
-                    <li key={name} className="flex items-center">
-                      <input
-                        type="checkbox"
-                        checked={!!selectedUsers[name]}
-                        onChange={() => handleCheckboxChange(name)}
-                        className="mr-2"
-                      />
-                      {name}
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            )}
+      <div >
+        <div className="w-3/4 p-8">
+          <div className="flex justify-between items-center mb-12">
+            <h2 className="text-4xl font-bold">Groups</h2>
             <button
-              className="mt-4 bg-blue-500 text-white px-4 py-2 rounded"
-              onClick={async () => {
-                // Continue here to write data to firebase. selectedUsers variable contains email id.
-              }}
+              className="bg-green-500 text-white px-5 py-5 rounded"
+              onClick={() => setShowSearchBox(!showSearchBox)}
             >
-              Submit
+              + Create
             </button>
           </div>
-        )}
 
-        {checkedEmails.length > 0 && (
-          <div className="mt-8">
-            <h3 className="text-xl font-semibold mb-4">Selected Emails:</h3>
-            <ul className="bg-gray-100 p-4 rounded shadow-md">
-              {checkedEmails.map((email) => (
-                <li key={email} className="border-b py-2">
-                  {email}
-                </li>
-              ))}
-            </ul>
-          </div>
-        )}
+          {showSearchBox && (
+            <div className="relative">
+              <input
+                type="text"
+                value={searchValue}
+                onChange={(e) => setSearchValue(e.target.value)}
+                placeholder="Search users..."
+                className="w-full px-4 py-2 rounded border shadow-md mb-4"
+              />
+              {filteredNames.length > 0 && (
+                <div className="absolute top-full w-full max-h-64 overflow-y-auto border">
+                  <ul>
+                    {filteredNames.map((name) => (
+                      <li key={name} className="flex items-center">
+                        <input
+                          type="checkbox"
+                          checked={!!selectedUsers[name]}
+                          onChange={() => handleCheckboxChange(name)}
+                          className="mr-2"
+                        />
+                        {name}
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              )}
+              <button
+                className="mt-4 bg-blue-500 text-white px-4 py-2 rounded"
+                onClick={async () => {
+                  // Continue here to write data to firebase. selectedUsers variable contains email id.
+                }}
+              >
+                Submit
+              </button>
+            </div>
+          )}
+
+          {checkedEmails.length > 0 && (
+            <div className="mt-8">
+              <h3 className="text-xl font-semibold mb-4">Selected Emails:</h3>
+              <ul className="bg-gray-100 p-4 rounded shadow-md">
+                {checkedEmails.map((email) => (
+                  <li key={email} className="border-b py-2">
+                    {email}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
+        </div>
+
+
       </div>
     </div>
   );
