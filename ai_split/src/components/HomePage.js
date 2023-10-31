aae8f09892ae344e30daf45fc94b7d4fd41f78cb
Ali <moinali5599@gmail.com>
2023-10-30 23:08:27 -0500

    home page ui

--- ai_split/src/components/HomePage.js
+++ ai_split/src/components/HomePage.js
@@ -1,46 +1,35 @@
-import {React, useContext} from "react";
+import { React, useContext } from "react";
 import { signOut } from "firebase/auth";
 import { auth } from "../firebase";
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../contexts/AuthContext";
 import SideNavigation from "./SideNavigation";
 import Profile from "./profile";
-import MemberComponent from "./memberHome"
-import BalanceComponent from "./BalamceHome"
-import '../App.css';
+import MemberComponent from "./memberHome";
+import BalanceComponent from "./BalamceHome";
+import "../App.css";
 import { handleLogout } from "./FireBaseFunc";
 function HomePage() {
-  
   const { setcurrentUser } = useContext(AuthContext);
   const history = useNavigate();
-  
+
   const handleLogoutClick = () => {
     handleLogout(auth, setcurrentUser, history);
-  }
+  };
 
   return (
-    <div className="flex">
+    <div className="flex overflow-hidden">
       <div className="w-1/4">
         <SideNavigation />
       </div>
-      
-      
-       <div className="app">
-       <div className="flex justify-between w-full">
-       <h2 className="text-4xl font-bold">Home</h2>
-      
-      <button className="w-24 h-15 bg-red text-smokewhite" onClick={handleLogoutClick}>Log Out</button>
-      </div>
-      <BalanceComponent amount={70.11} />
-      <br>
-      </br>
-      <br></br>
-      <h2 className="text-4xl font-bold">Group Name</h2>
-      <MemberComponent name="Member 1" amount={23.43} />
+
+      <div className="w-3/4 px-16 bg-smokewhite">
+        <BalanceComponent amount={70.11} />
+        <div className="bg-white px-10 py-6 rounded-lg mt-12 h-3/5 overflow-scroll">
+          <MemberComponent name="Member 1" amount={23.43} />
+        </div>
       </div>
-     
     </div>
-    
   );
 }
 
--- ai_split/src/components/memberHome.js
+++ ai_split/src/components/memberHome.js
@@ -5,16 +5,18 @@ import Member_3 from '../member3.png';
 
 
 const members = ["Member_1"];
-const mempic = [Member_1, Member_2, Member_3];
+const mempic = [Member_1, Member_2, Member_3, Member_1,Member_3];
 
 const memberHome = () => {
     return mempic.map(mempic => (
-         <div className=" mb-4 flex items-center space-x-8">
+         <div className=" mb-6 flex items-center space-x-8">
             < img className ="w-20 h-20 rounded-full ml-4" src={mempic} alt={members}  />
-            
             <div  className =" text-2xl font-italic flex-grow ">{members}</div>
-            <span></span>
-            <p>You owe $23.43</p>
+            
+            <div className='flex flex-col items-center'>
+                <p>You owe</p>
+                <p className='text-xl font-semibold text-dark-green'>$23.43</p>
+            </div>
         </div>
     ));
 }
